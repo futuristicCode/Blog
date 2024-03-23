bbe1941eff8773a220e9d15ab565b981b121cfb5
futuristicCode <baoumaramadoubocar@gmail.com>
2024-03-23 02:27:50 +0000

    Create a features CRUD  for  POST DONE

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post, Req, UseGuards } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, Req, UseGuards } from "@nestjs/common";
 import { PostService } from "./post.service";
 import { AuthGuard } from "@nestjs/passport";
-import { CreatePostDto } from "./dto/createPostDto";
-import { Request } from "express";
+import { CreatePostDto } from './dto/createPostDto';
+import { Request, request } from "express";
+import { UpdatePostDto } from "./dto/updatePostDto";
 
 @Controller('posts')
 export class PostController {
@@ -16,9 +17,27 @@ export class PostController {
     const userId = request.user["userId"]
     return this.postService.createPost(createPostDto, userId)
   }
+
   // ** UPDATE POST
+  @UseGuards(AuthGuard('jwt'))
+  @Put(':id')
+   updatePostDto(@Param("id", ParseIntPipe) postId: number,@Body() updatePostDto: UpdatePostDto, @Req() request: Request) {
+    const userId = request.user['userId']
+    return this.postService.updatePost(postId,userId,updatePostDto) 
+  }
   // ** DELETE POST
+  @UseGuards(AuthGuard('jwt'))
+  @Delete(':id')
+  deletePostDto(@Param("id", ParseIntPipe) postId: number, @Req() request: Request) {
+    const userId = request.user['userId']
+    return this.postService.deletePost(postId,userId)
+  }
   // ** GET POST BY ID
   // ** GET ALL POST
+  @Get()
+  getAllPosts() {
+    const posts = this.postService.getAllPosts()
+    return posts
+  }
 
 }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,16 +1,74 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePostDto } from './dto/createPostDto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { Exclude } from 'class-transformer';
+import { UpdatePostDto } from './dto/updatePostDto';
 
 @Injectable()
 export class PostService {
 
     constructor(private readonly prismaService: PrismaService) { }
-    
+
+
+
+    // ** CREATE POST ** //
     async createPost(createPostDto: CreatePostDto, userId: any) {
         const { body, title, image } = createPostDto;
         const authorId = userId
-        await this.prismaService.post.create({ data : {body,image,title,authorId}});
-        return { data : "Post Created !" };
+        await this.prismaService.post.create({ data: { body, image, title, authorId } });
+        return { data: "Post Created !" };
     }
+    
+    // ** UPDATE POST ** //
+    async updatePost(postId:number,authorId:number,updatePostDto: UpdatePostDto) {
+        const post = await this.prismaService.post.findUnique({ where: { postId } })
+        if (!post) throw new NotFoundException("Post Does Not Exists Or Not Found")
+        if (post.authorId != authorId) throw new ForbiddenException("Forbidden Action") 
+        await this.prismaService.post.update({ where: { postId }, data: { ...updatePostDto } })
+        return {data: "Post Update!"}
+    }
+
+    // ** DELETE POST ** //
+    async deletePost(postId:number,authorId:number) {
+        const post = await this.prismaService.post.findUnique({ where: { postId } })
+        if (!post) throw new NotFoundException("Post Does Not Exists Or Not Found")
+        if (post.authorId != authorId) throw new ForbiddenException("Forbidden Action")
+        await this.prismaService.post.delete({ where: { postId } }
+        )
+        return {data : "Post Deleted!"}
+    }
+
+    //** GET ALL POST **/
+
+    async getAllPosts() {
+        
+        const posts = await this.prismaService.post.findMany({
+             include: {
+                author: {
+                     select: {
+                         userId:true,
+                        username: true,
+                        email: true,
+                        password: false,
+                        created_at: true,
+                        updated_at: true,
+                         role:true
+                     }
+                 },
+                 comment: true,
+                 categories: true
+
+             }  
+        })
+        
+        console.log(posts)
+        return posts
+    }
+
+    //**     GET POST BY ID */
+
+    async getPost() {
+        
+    }
+
 }
