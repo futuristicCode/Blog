e89443bf17222e9c193be7d9751d9012fa9af9f9
futuristicCode <baoumaramadoubocar@gmail.com>
2024-03-24 03:48:50 +0000

    installation swagger for API documentation and adding swagger plugin on nest-cli.json file and  rename DTO files

---
+++ src/auth/dto/resetPasswordDemand.dto.ts
@@ -0,0 +1,8 @@
+import { IsEmail, IsNotEmpty } from "class-validator";
+
+export class ResetPasswordDemandDto {
+  
+  @IsNotEmpty()
+  @IsEmail()
+  readonly email: string
+}
---
+++ src/post/dto/createPost.dto.ts
@@ -0,0 +1,13 @@
+import { ApiProperty } from "@nestjs/swagger"
+import { IsNotEmpty } from "class-validator"
+
+
+export class CreatePostDto {
+  @IsNotEmpty()
+  readonly image: string[]
+  @IsNotEmpty()
+  readonly title: string
+  @IsNotEmpty()
+  readonly body: string  
+
+}
